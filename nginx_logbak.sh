ea1c54d2d8c87937e19dec75de9d51cd329224e9
yangxin <50073307+XinYangJuly@users.noreply.github.com>
2019-06-04 22:16:59 -0500

    Update nginx_logbak.sh

--- nginx_logbak.sh
+++ nginx_logbak.sh
@@ -3,7 +3,7 @@ date=`date +%Y%m%d`
 logpath=`/usr/local/nginx/logs`
 mv $logpath/access.log $logpath/access-$date.log
 mv $logpath/access.log $logpath/error-$date.log
-kill -USR1 $(cat $logpath/nginx.pid)
+kill --USR1 $(cat $logpath/nginx.pid)
 
 crontab -e
 * * * * * bash /usr/local/nginx/logs/nginx_logbak.sh
