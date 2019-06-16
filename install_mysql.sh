8792dacb618aeca7a5abd183dbd62ab2809162bc
SMALL ROOM <50073307+XinYangJuly@users.noreply.github.com>
2019-06-16 02:02:08 -0500

    install_mysql.sh
    安装MySQL并完成初次修改密码

---
+++ install_mysql.sh
@@ -0,0 +1,25 @@
+read -p "请输入ip的网络位如192.168.4.i :" Network
+read -p "for循环值ip主机位值,如:50 多台{50..55}" num
+for i in $num
+do
+     scp /linux-soft/03/mysql/mysql-5.7.17.tar ${Network}$i:/root/
+done
+for i in $num
+do
+    ssh ${Network}$i "mkdir mysql; mv mysql-5.7.17.tar mysql/; cd mysql; tar -xf mysql-5.7.17.tar; yum -y install mysql-comm*.rpm; systemctl start mysqld;"
+done
+for i in $num
+do
+    scp /linux-soft/03/mysql/libev-4.15-1.el6.rf.x86_64.rpm ${Network}${i}:/root/mysql
+    scp /linux-soft/03/mysql/percona-xtrabackup-24-2.4.7-1.el7.x86_64.rpm ${Network}$i:/root/mysql
+done
+for i in $num
+do
+expect << EOF
+spawn ssh ${Network}$i
+expect "#"   { send "mysql -uroot -p"`grep password /var/log/mysqld.log |awk '{print $NF}'`"\r" }
+expect ">"   { send "alter user root@"localhost" identified by "123qqq...A"\r" }
+expect ">"   { send "exit\r" }
+expect "#"   { send "exit\r" }
+EOF
+done
