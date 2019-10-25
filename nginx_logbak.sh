5c5aac1743c6ff06beba02b4e8f2f86687a0762e
LITTLE BOY <50073307+XinYangJuly@users.noreply.github.com>
2019-10-25 10:31:21 +0800

    Update nginx_logbak.sh

--- nginx_logbak.sh
+++ nginx_logbak.sh
@@ -5,5 +5,10 @@ mv $logpath/access.log $logpath/access-$date.log
 mv $logpath/access.log $logpath/error-$date.log
 kill --USR1 $(cat $logpath/nginx.pid)
 
-crontab -e
-* * * * * bash /usr/local/nginx/logs/nginx_logbak.sh
+# kill -USR1 等于 nginx -s reopen
+#这个信号量本来就是用于重新读取日志文件的
+#kill -USR2 等于 nginx -s reload
+#reload 和 reopen 的行为相差很大，reopen 仅仅检查日志文件，reload 会重载配置，并启动新 worker，关闭旧 worker
+
+#crontab -e
+#* * * * * bash /usr/local/nginx/logs/nginx_logbak.sh
